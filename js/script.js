5b658ff4b9892593ad69f4cae217cf864208f1fd
Mao <mowitz.almstedt@gmail.com>
2025-02-09 21:34:16 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Initialize Typed.js when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+    var typed = new Typed('#auto-type', {
+        strings:
+            ['Hello! Im a young student with a passion for programming and all things tech.'],
+        typeSpeed: 50,
+    });
+});
