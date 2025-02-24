4956658282cc4e1f4a784364e2b7522880574c5b
mao <mowitz.almstedt@gmail.com>
2025-02-24 11:35:53 +0100

    removed unnecessary files

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Initialize Typed.js when the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", () => {
-    var typed = new Typed('#auto-type', {
-        strings:
-            ['Hello! Im a young student with a passion for programming and all things tech.'],
-        typeSpeed: 50,
-    });
-});
